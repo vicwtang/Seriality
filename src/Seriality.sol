ee78055b44d9ba307920b6c5c981da405b733c80
vicwtang <68478211+vicwtang@users.noreply.github.com>
2022-03-05 01:48:46 -0800

    updated compiler

--- src/Seriality.sol
+++ src/Seriality.sol
@@ -1,18 +1,17 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.4;
+
+// SPDX-License-Identifier: APACHE 2.0
 
 /**
  * @title Seriality
  * @dev The Seriality contract is the main interface for serializing data using the TypeToBytes, BytesToType and SizeOf
  * @author pouladzade@gmail.com
  */
- 
-import "./BytesToTypes.sol";
-import "./TypesToBytes.sol";
-import "./SizeOf.sol";
+
+import './BytesToTypes.sol';
+import './TypesToBytes.sol';
+import './SizeOf.sol';
 
 contract Seriality is BytesToTypes, TypesToBytes, SizeOf {
-
-    constructor() public {
-
-    }
+	constructor() {}
 }
--- src/TypesToBytes.sol
+++ src/TypesToBytes.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.4;
+
+// SPDX-License-Identifier: APACHE 2.0
 
 /**
  * @title TypesToBytes
@@ -7,57 +9,78 @@ pragma solidity ^0.5.0;
  */
 
 contract TypesToBytes {
- 
-    constructor() internal {
-        
-    }
-    function addressToBytes(uint _offst, address _input, bytes memory _output) internal pure {
+	constructor() {}
 
-        assembly {
-            mstore(add(_output, _offst), _input)
-        }
-    }
+	function addressToBytes(
+		uint256 _offst,
+		address _input,
+		bytes memory _output
+	) internal pure {
+		assembly {
+			mstore(add(_output, _offst), _input)
+		}
+	}
 
-    function bytes32ToBytes(uint _offst, bytes32 _input, bytes memory _output) internal pure {
+	function bytes32ToBytes(
+		uint256 _offst,
+		bytes32 _input,
+		bytes memory _output
+	) internal pure {
+		assembly {
+			mstore(add(_output, _offst), _input)
+			mstore(add(add(_output, _offst), 32), add(_input, 32))
+		}
+	}
 
-        assembly {
-            mstore(add(_output, _offst), _input)
-            mstore(add(add(_output, _offst),32), add(_input,32))
-        }
-    }
-    
-    function boolToBytes(uint _offst, bool _input, bytes memory _output) internal pure {
-        uint8 x = _input == false ? 0 : 1;
-        assembly {
-            mstore(add(_output, _offst), x)
-        }
-    }
-    
-    function stringToBytes(uint _offst, bytes memory _input, bytes memory _output) internal pure {
-        uint256 stack_size = _input.length / 32;
-        if(_input.length % 32 > 0) stack_size++;
-        
-        assembly {
-            stack_size := add(stack_size,1)//adding because of 32 first bytes memory as the length
-            for { let index := 0 } lt(index,stack_size){ index := add(index ,1) } {
-                mstore(add(_output, _offst), mload(add(_input,mul(index,32))))
-                _offst := sub(_offst , 32)
-            }
-        }
-    }
+	function boolToBytes(
+		uint256 _offst,
+		bool _input,
+		bytes memory _output
+	) internal pure {
+		uint8 x = _input == false ? 0 : 1;
+		assembly {
+			mstore(add(_output, _offst), x)
+		}
+	}
 
-    function intToBytes(uint _offst, int _input, bytes memory  _output) internal pure {
+	function stringToBytes(
+		uint256 _offst,
+		bytes memory _input,
+		bytes memory _output
+	) internal pure {
+		uint256 stack_size = _input.length / 32;
+		if (_input.length % 32 > 0) stack_size++;
 
-        assembly {
-            mstore(add(_output, _offst), _input)
-        }
-    } 
-    
-    function uintToBytes(uint _offst, uint _input, bytes memory _output) internal pure {
+		assembly {
+			stack_size := add(stack_size, 1) //adding because of 32 first bytes memory as the length
+			for {
+				let index := 0
+			} lt(index, stack_size) {
+				index := add(index, 1)
+			} {
+				mstore(add(_output, _offst), mload(add(_input, mul(index, 32))))
+				_offst := sub(_offst, 32)
+			}
+		}
+	}
 
-        assembly {
-            mstore(add(_output, _offst), _input)
-        }
-    }   
+	function intToBytes(
+		uint256 _offst,
+		int256 _input,
+		bytes memory _output
+	) internal pure {
+		assembly {
+			mstore(add(_output, _offst), _input)
+		}
+	}
 
+	function uintToBytes(
+		uint256 _offst,
+		uint256 _input,
+		bytes memory _output
+	) internal pure {
+		assembly {
+			mstore(add(_output, _offst), _input)
+		}
+	}
 }
